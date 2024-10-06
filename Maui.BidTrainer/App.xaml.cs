6fdca396cf7bb61d3d26c2f66bce19b810784454
Yke Smit <y.smit@xs4all.nl>
2024-10-06 12:08:38 +0200

    Set screensize on windows

--- Maui.BidTrainer/App.xaml.cs
+++ Maui.BidTrainer/App.xaml.cs
@@ -8,5 +8,21 @@
 
             MainPage = new AppShell();
         }
+        
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+#if WINDOWS
+        window.Width = 600;
+        window.Height = 1000;
+        
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+        window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+        window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+#endif
+            return window;
+        }    
+        
     }
 }
