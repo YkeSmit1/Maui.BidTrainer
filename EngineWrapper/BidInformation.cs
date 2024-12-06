6c52da284a6119472bf8355310b524fde2232e94
Yke Smit <y.smit@xs4all.nl>
2024-12-07 00:05:40 +0100

    Refactoring

--- EngineWrapper/BidInformation.cs
+++ EngineWrapper/BidInformation.cs
@@ -16,20 +16,23 @@ public class BidInformation
             
     public BidInformation(List<Dictionary<string, string>> records)
     {
-        HasInformation = records.Any();
-        minRecords = records.SelectMany(x => x).Where(x => x.Key.StartsWith("Min")).GroupBy(x => x.Key).ToDictionary(g => g.Key, g => g.Select(x => int.Parse(x.Value)).Min());
-        maxRecords = records.SelectMany(x => x).Where(x => x.Key.StartsWith("Max")).GroupBy(x => x.Key).ToDictionary(g => g.Key, g => g.Select(x => int.Parse(x.Value)).Max());
+        HasInformation = records.Count != 0;
+        minRecords = records.SelectMany(x => x).Where(x => x.Key.StartsWith("Min")).GroupBy(x => x.Key)
+            .ToDictionary(g => g.Key, g => g.Select(x => int.Parse(x.Value)).Min());
+        maxRecords = records.SelectMany(x => x).Where(x => x.Key.StartsWith("Max")).GroupBy(x => x.Key)
+            .ToDictionary(g => g.Key, g => g.Select(x => int.Parse(x.Value)).Max());
         ids = records.SelectMany(x => x).Where(x => x.Key == "Id").Select(x => Convert.ToInt32(x.Value)).ToList();
-        controls = records.Any() 
+        controls = records.Count != 0
             ? [GetHasProperty("SpadeControl"), GetHasProperty("HeartControl"), GetHasProperty("DiamondControl"), GetHasProperty("ClubControl")] 
             : [null, null, null, null];
         possibleKeyCards = records.SelectMany(x => x).Where(x => x.Key == "KeyCards" && !string.IsNullOrWhiteSpace(x.Value)).Select(x => int.Parse(x.Value)).ToList();
-        trumpQueen = records.Any() ? GetHasProperty("TrumpQueen") : null;
+        trumpQueen = records.Count != 0 ? GetHasProperty("TrumpQueen") : null;
+        return;
 
         bool? GetHasProperty(string fieldName)
         {
             var recordsWithValue = records.SelectMany(x => x).Where(x => x.Key == fieldName && !string.IsNullOrWhiteSpace(x.Value)).ToList();
-            var p = !recordsWithValue.Any() ? null : (bool?)(int.Parse(recordsWithValue.FirstOrDefault().Value) == 1);
+            var p = recordsWithValue.Count == 0 ? null : (bool?)(int.Parse(recordsWithValue.FirstOrDefault().Value) == 1);
             return p;
         }
     }
@@ -51,7 +54,7 @@ public class BidInformation
                 return "";
             var stringBuilder = new StringBuilder();
             stringBuilder.Append("\nControls: ");
-            foreach (var suit in Enum.GetValues(typeof(Suit)).Cast<Suit>().Except(new[] { Suit.NoTrump }))
+            foreach (var suit in Enum.GetValues(typeof(Suit)).Cast<Suit>().Except([Suit.NoTrump]))
                 if (controls[3 - (int)suit].GetValueOrDefault())
                     stringBuilder.Append(suit);
             return stringBuilder.ToString();
@@ -59,9 +62,7 @@ public class BidInformation
 
         string GetKeyCardsAsText()
         {
-            if (!possibleKeyCards.Any())
-                return "";
-            return $"\nKeyCards: {string.Join(",", possibleKeyCards)}";
+            return possibleKeyCards.Count == 0 ? "" : $"\nKeyCards: {string.Join(",", possibleKeyCards)}";
         }
 
         string GetTrumpQueen()
