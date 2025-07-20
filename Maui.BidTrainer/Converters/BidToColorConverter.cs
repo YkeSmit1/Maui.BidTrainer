91231f99d6893c4c37c4242565f53b01005b22a2
Yke Smit <y.smit@xs4all.nl>
2025-07-20 23:08:20 +0200

    Now hearts and diamonds in the biddingbox are red again

---
+++ Maui.BidTrainer/Converters/BidToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Common;
+
+namespace Maui.BidTrainer.Converters;
+
+public class BidToColorConverter: IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if  (value is not Bid bid)
+            return null;
+        return bid.Suit is Suit.Diamonds or Suit.Hearts ? Colors.Red :
+            Application.Current?.RequestedTheme == AppTheme.Light ? Colors.Black : Colors.White;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
