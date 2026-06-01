3fad030c484b486a1e8c88f9407727a6d1274b6d
Yke Smit <yke.smit@gmail.com>
2026-06-01 17:53:10 +0200

    Added a dummy implementation of the leaderboard disabled some warnings

---
+++ Maui.BidTrainer/Platforms/iOS/CosmosDBHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Maui.BidTrainer.Platforms.IOS;
+
+public class CosmosDbHelper : ICosmosDbHelper
+{
+
+    public async Task<IEnumerable<Account>> GetAllAccounts()
+    {
+        return await Task.FromResult(new List<Account>());
+    }
+
+    public Task<Account?> GetAccount(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertAccount(Account account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAccount(Account account)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Maui.BidTrainer/Platforms/iOS/Program.cs
+++ Maui.BidTrainer/Platforms/iOS/Program.cs
@@ -10,6 +10,7 @@ namespace Maui.BidTrainer.Platforms.IOS
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, typeof(AppDelegate));
+            DependencyService.Register<ICosmosDbHelper, CosmosDbHelper>();
         }
     }
 }
